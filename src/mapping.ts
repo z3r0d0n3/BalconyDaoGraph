d1933fc0047a0d993d68e7189431237944328078
Void <c4rm3k@gmail.com>
2022-03-25 09:23:45 +0200

    up

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,33 +5,33 @@ import {
   ApprovalForAll,
   NewDeed,
   Transfer,
-  UpdateDeed
+  UpdateDeed,
 } from "../generated/BalconyDeed/BalconyDeed"
-import { ExampleEntity } from "../generated/schema"
+import { Participant, Deed } from "../generated/schema"
 
 export function handleApproval(event: Approval): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  // let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  // // Entities only exist after they have been saved to the store;
+  // // `null` checks allow to create entities on demand
+  // if (!entity) {
+  //   entity = new ExampleEntity(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  //   // Entity fields can be set using simple assignments
+  //   entity.count = BigInt.fromI32(0)
+  // }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // // BigInt and BigDecimal math are supported
+  // entity.count = entity.count + BigInt.fromI32(1)
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
+  // // Entity fields can be set based on event parameters
+  // entity.owner = event.params.owner
+  // entity.approved = event.params.approved
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  // // Entities can be written to the store with `.save()`
+  // entity.save()
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -70,8 +70,34 @@ export function handleApproval(event: Approval): void {
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
-export function handleNewDeed(event: NewDeed): void {}
+export function handleNewDeed(event: NewDeed): void {
+  let participant = Participant.load(event.params.DeedOwner.toHex())
+  let contract = BalconyDeed.bind(event.address)
+
+  if (!participant) {
+    participant = new Participant(event.params.DeedOwner.toHex())
+    participant.address = event.params.DeedOwner
+   
+  }
+  let deed = contract.addressToDeed(event.params.DeedOwner)
+
+  participant.points = deed.value2
+  participant.save()
+
+
+  let referer = Participant.load(event.params.Referer.toHex());
+
+  if (!referer) {
+    referer = new Participant(event.params.Referer.toHex())
+    referer.address = event.params.Referer
+  }
+  let deed1 = contract.addressToDeed(event.params.Referer)
+  referer.points = deed1.value2;
+  referer.save()
+
+}
 
 export function handleTransfer(event: Transfer): void {}
-
+  
 export function handleUpdateDeed(event: UpdateDeed): void {}
+  
